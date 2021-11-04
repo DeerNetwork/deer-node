9cd4a413e5d76afddede061f5c7842a55420a4a9
sigoden <sigoden@gmail.com>
2021-11-04 21:43:31 +0800

    Upgrade runtime to 230 (#24)
    * Upgrade runtime to 230
    
    * update pallet weights

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -97,7 +97,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 220,
+	spec_version: 230,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
