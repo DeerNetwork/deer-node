2c9a0c82dd9b9877ec2b1d633af159958baf62d1
sigoden <sigoden@gmail.com>
2022-01-07 12:26:13 +0800

    Add price paramteter to NFT.TransferredToken event (#52)

--- pallets/nft/src/benchmarking.rs
+++ pallets/nft/src/benchmarking.rs
@@ -138,7 +138,7 @@ benchmarks_instance_pallet! {
 		let target_lookup = T::Lookup::unlookup(target.clone());
 	}: _(SystemOrigin::Signed(caller.clone()), class_id, token_id, quantity, target_lookup)
 	verify {
-		assert_last_event::<T, I>(Event::TransferredToken { class_id, token_id, quantity, from: caller, to: target, reason: TransferReason::Direct }.into());
+		assert_last_event::<T, I>(Event::TransferredToken { class_id, token_id, quantity, from: caller, to: target, reason: TransferReason::Direct, price: Zero::zero() }.into());
 	}
 }
 
