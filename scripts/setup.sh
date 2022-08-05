dc29d2eae56b309af81c265539beb26c02d9b705
sigoden <sigoden@gmail.com>
2022-08-05 23:07:24 +0800

    chore: update readme and setup.sh (#58)
    * chore: update readme and setup.sh
    
    * update argcfile

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,7 @@ sudo apt update
 sudo apt install -y git clang curl libssl-dev llvm libudev-dev
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source ~/.bashrc
+. "$HOME/.cargo/env"
 
-rustup toolchain add nightly-2021-11-30
-rustup target add wasm32-unknown-unknown --toolchain nightly-2021-11-30
+rustup toolchain add nightly-2022-07-11
+rustup target add wasm32-unknown-unknown --toolchain nightly-2022-07-11
